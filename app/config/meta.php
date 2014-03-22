38885b693f70142b425cf9aec67c847ce6d8ab41
Troy Clark <raptblue@gmail.com>
2014-03-22 11:18:21 -0400

    add meta config for displaying version in the footer

---
+++ app/config/meta.php
@@ -0,0 +1,16 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Application Version
+	|--------------------------------------------------------------------------
+	|
+	| Set the version string.  Used in the footer and other various (usually
+	| debugging or admin) sections of the site.
+	|
+	*/
+
+    'version' => '0.0.1-alpha',
+);
